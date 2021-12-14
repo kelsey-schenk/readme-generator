786b57093466ca3babff24836c4f7f00a882dada
kelsey-schenk <kelsey.schenk@att.net>
2021-12-14 18:06:26 -0500

    init function

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generateReadme = require()
+const path = require('path');
+const generateReadme = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const readmeQuestions = readmeData => {
+function readmeQuestions () {
     return inquirer.prompt([
         {
             type: 'input',
@@ -91,10 +108,17 @@ const readmeQuestions = readmeData => {
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // Write file sync needs two parameters, first is path, second is what we're putting into the file
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    readmeQuestions().then((response)=>{
+        writeToFile('ReadMeMarkdown.md', generateReadme({...response}));
+    })
+}
 
 // Function call to initialize app
-readmeQuestions ();
+init ();
